9e39dcbe13ed40f6fa15dab6b0a766f384ddbf82
SchrageCh <Christian.Schrage@nefkom.info>
2013-11-14 16:33:17 +0100

    php

--- KJG_APP/index.php
+++ KJG_APP/index.php
@@ -14,6 +14,26 @@
     <!-- Custom styles for this template 
     <link href="./css/navbar-static-top.css" rel="stylesheet">-->
 
+	<?php
+		$zeiger = fopen("https://www.google.com/calendar/feeds/kjg.altenfurt%40googlemail.com/private-35c9e90434f5ffee5da7463d454f89fb/basic", "r");
+		$seite;
+		if ($zeiger) 
+		{
+			while (($buffer = fgets($zeiger)) !== false) 
+			{
+				$seite = $seite . $buffer;
+			}
+			if (!feof($zeiger)) 
+			{
+				echo "Fehler: unerwarteter fgets() Fehlschlag\n";
+			}
+			fclose($zeiger);
+		}
+		echo $seite;
+
+		#$teil = substr($inhalt, strpos($inhalt, "<title type='html'>"), strpos($inhalt, "<title type='html'>") - strpos($inhalt, "</title>"));
+		#echo $teil;
+	?>
   </head>
 
   <body>
